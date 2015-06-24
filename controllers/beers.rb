28325e3c383b7b6b14e88e30bda70d043b3ff934
David Gardetti <dgardetti@gmail.com>
2015-06-24 13:35:32 -0500

    added 'delete beer' menu

--- controllers/beers.rb
+++ controllers/beers.rb
@@ -51,6 +51,23 @@ get "/beer/edit_beer_name" do
   end
 end
 
+# --------------------------------------------- Delete Beers ------------------------------------------------------
+
+get "/beer/delete_beer_form" do
+  @all_beers = Beer.all
+  erb :"beers/delete_beer_form"
+end
+
+get "/beer/delete_beer" do
+  @beer_to_delete = Beer.find_as_object(params["beer_id"].to_i)
+  if @beer_to_delete.delete
+    erb :"beers/beer_deleted"
+  else
+    @error = true
+    erb :"beers/edit_beer_form"
+  end
+end
+
 
 
 
