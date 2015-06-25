4c6d9b07619d9c36bec160f07cbc3e14af52c998
David Gardetti <dgardetti@gmail.com>
2015-06-25 11:17:48 -0500

    added user added confirm page

--- controllers/users.rb
+++ controllers/users.rb
@@ -18,7 +18,7 @@ get "/users/add" do
   new_user_id = User.add({"user_name" => params["user_name"]})
   
   if new_user_id
-    @new_user = User.find(new_beer_id)
+    @new_user = User.find(new_user_id)
     erb :"users/user_added"
   else
     @error = true
@@ -29,19 +29,19 @@ end
 # --------------------------------------------- View Users --------------------------------------------------------
 
 get "/beer/view_all_beers" do
-  @all_beers = Beer.all
+  @all_users = User.all
   erb :"beers/view_beers"
 end
 
 # --------------------------------------------- Edit Users --------------------------------------------------------
 
 get "/beer/edit_beers" do
-  @all_beers = Beer.all
+  @all_beers = User.all
   erb :"beers/edit_beer_form"
 end
 
 get "/beer/edit_beer_name" do
-  @beer_to_edit = Beer.find(params["beer_id"].to_i)
+  @beer_to_edit = User.find(params["beer_id"].to_i)
   @beer_to_edit.beer_name = params["beer_name"]
   if @beer_to_edit.save
     erb :"beers/beer_name_changed"
@@ -54,12 +54,12 @@ end
 # --------------------------------------------- Delete Users ------------------------------------------------------
 
 get "/beer/delete_beer_form" do
-  @all_beers = Beer.all
+  @all_beers = User.all
   erb :"beers/delete_beer_form"
 end
 
 get "/beer/delete_beer" do
-  @beer_to_delete = Beer.find(params["beer_id"].to_i)
+  @beer_to_delete = User.find(params["beer_id"].to_i)
   if @beer_to_delete.delete
     erb :"beers/beer_deleted"
   else
